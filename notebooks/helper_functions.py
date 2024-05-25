a6e398eb8a5c22cd32dba56e782b208c81a8c4b4
Teresa Whitesell <tfwhitesell@gmail.com>
2024-05-25 06:40:18 -0500

    pre-processing

--- notebooks/helper_functions.py
+++ notebooks/helper_functions.py
@@ -1,10 +1,22 @@
-def read_text(foldername:str, work_id:str):
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import regexp_tokenize
+from collections import Counter
+
+def read_text(work_id:str):
     '''
     Open text file in read-only mode.
-    Input folder and work_id to retrieve contents.
+    Input work_id to retrieve contents.
     '''
-    with open(f'../data/{foldername}/{work_id}.txt', 'r') as file:
-        print(work_id)
+    with open(f'../data/texts/{work_id}.txt', 'r') as file:
         data = file.read()
     
     return data
+
+def get_counter(text:str):
+    '''Create counter object from text.'''
+    stop_words = set(stopwords.words('english'))
+    wnl = WordNetLemmatizer()
+    text_counter = Counter([wnl.lemmatize(x.lower()) for x in regexp_tokenize(text, '[-\'\w]+') if x.lower() not in stop_words])
+
+    return text_counter
